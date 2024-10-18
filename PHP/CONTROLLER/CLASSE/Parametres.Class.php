c95058d43d69fc8d3dca9a2b02dab898f4e13db9
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-18 12:07:04 -0600

    fix init

--- PHP/CONTROLLER/CLASSE/Parametres.Class.php
+++ PHP/CONTROLLER/CLASSE/Parametres.Class.php
@@ -33,24 +33,9 @@ class Parametres
 	}
 
 
-	// static function init()
-	// {
-	// 	if (file_exists("config.json")) {
-	// 		$parametre  = json_decode(file_get_contents("config.json"));
-	// 		self::$_host = $parametre->Host;
-	// 		self::$_port = $parametre->Port;
-	// 		self::$_dbname = $parametre->DbName;
-	// 		self::$_login = $parametre->Login;
-	// 		if (strlen($parametre->Pwd) == 0)
-	// 			self::$_pwd = $parametre->Pwd; //developpement
-	// 		else
-	// 			self::$_pwd = $parametre->Pwd; //production
-	// 	}
-	// }
 	static function init() {
 		// Check if the config.php file exists
 		if (file_exists("config.php")) {
-			// Include the config.php file and store the returned array in $parametre
 			include "config.php";
 			// Access the configuration values
 			self::$_host = $parameters['Host'];
