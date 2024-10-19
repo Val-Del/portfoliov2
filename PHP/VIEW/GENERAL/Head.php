40fcab3f0325deb1bcaa96ddfa57d0ff71fca7c9
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-19 14:15:28 -0600

    typo

--- PHP/VIEW/GENERAL/Head.php
+++ PHP/VIEW/GENERAL/Head.php
@@ -7,7 +7,7 @@ echo '<title>Valentin Bruneel</title>';
 echo '<link rel="icon" type="image/x-icon" href="./IMG/s.png">';
 echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />';
 echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
-echo '<meta name="description" content="Portefolio of Valentin Bruneel" />';
+echo '<meta name="description" content="Portfolio of Valentin Bruneel" />';
 echo'
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
     integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
