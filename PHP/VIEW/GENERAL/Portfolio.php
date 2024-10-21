c91fe8d9c09c2574733dfc012c16c5618e1ad63a
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-21 14:57:27 -0600

    add view .me .work

--- PHP/VIEW/GENERAL/Portfolio.php
+++ PHP/VIEW/GENERAL/Portfolio.php
@@ -1,11 +1,31 @@
 <main class="main flex">
     <div class="left"></div>
     <div class="content">
-        <div class="me">
+        <section class="me">
             <h3 class="title">
-                Developer
+                Full Stack Developer
             </h3>
-        </div>
+            <p class="description">
+                Hello! I'm a Full Stack Developer with a strong foundation in application development and web design.
+            </p>
+        </section>
+        <section class="work flex">
+            <?php
+            $works = WorksManager::getList();
+            if ($works) {
+                foreach ($works as $work) {
+                    if ($work->getVisible()) {
+                        echo '<div class=card>';
+                                echo '<img src=IMG/'.$work->getThumbnail().'></img>';
+                                echo '<div class=overlay>';
+                                    echo '<h4>'.$work->getName().'</h4>';
+                                echo '</div>';
+                        echo '</div>';
+                    }
+                }
+            }
+            ?>
+        </section>
     </div>
     <div class="right"></div>
 </main>
