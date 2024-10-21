809e3e8546e32294e84ef5ba29ac6de20a90ec89
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-21 12:13:52 -0600

    add portfolio view

---
+++ PHP/VIEW/GENERAL/Portfolio.php
@@ -0,0 +1,11 @@
+<main class="main flex">
+    <div class="left"></div>
+    <div class="content">
+        <div class="me">
+            <h3 class="title">
+                Developer
+            </h3>
+        </div>
+    </div>
+    <div class="right"></div>
+</main>
