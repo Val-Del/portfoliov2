3a434e896c274c61c44db1100b5947708c53da81
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-28 13:56:04 -0600

    add isset and update nav

--- PHP/VIEW/GENERAL/HeaderPortfolio.php
+++ PHP/VIEW/GENERAL/HeaderPortfolio.php
@@ -3,10 +3,30 @@
         <div class="left"></div>
         <div class="content flex">
             <nav class=main>
+                <?php 
+                $portefolio = $resume = '';
+                if (isset($_GET['page'])) {
+                    $page = preg_replace('/[^a-zA-Z0-9_-]/', '', $_GET['page']);
+                    switch ($page) {
+                        case 'Portfolio':
+                            $portefolio = 'highlight';
+                            break;
+                        case 'Resume':
+                            $resume = 'highlight';
+                            break;
+                        default:
+                            $portefolio = 'highlight';
+                            break;
+                    }
+                }else {
+                    $portefolio = 'highlight';
+                }
+               
+                ?>
                 <ul class=flex>
-                    <li>Work</li>
-                    <li>Resume</li>
-                    <li>Contact</li>
+                     <li><a class="<?= $portefolio ?>" href="?page=Portfolio">Work</a></li>
+                    <li><a class="<?= $resume ?>"  target="_blank" href="?page=Resume">Resume</a></li>
+                    <!-- <li><a>Contact</a></li> -->
                 </ul>
             </nav>
             <h1 class="name">
