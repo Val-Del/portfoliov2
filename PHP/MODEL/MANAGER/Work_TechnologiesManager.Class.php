23c98ea2979d221010be7958d6de89d32ce10ae6
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-28 13:54:43 -0600

    add JOIN to manager

--- PHP/MODEL/MANAGER/Work_TechnologiesManager.Class.php
+++ PHP/MODEL/MANAGER/Work_TechnologiesManager.Class.php
@@ -22,6 +22,28 @@ class Work_technologiesManager
         return DAO::select(Work_technologies::getAttributes(), "work_technologies", ["technology_id" => $id])[0];
     }
 
+
+  public static function findTechnologiesByWorkId($workId) {
+    // Columns we want to select (from the technologies table)
+    $columns = ['technologies.id', 'technologies.name'];
+    
+    // Define the JOIN between work_technologies and technologies
+    $joins = [
+        'technologies' => 'technologies.id = work_technologies.technology_id'
+    ];
+    
+    // Conditions for the query
+    $conditions = [
+        'work_technologies.work_id' => $workId
+    ];
+    
+    // Use the secure selectWithJoin method to execute the query
+    return DAO::selectWithJoin($columns, 'work_technologies', $joins, $conditions);
+    }
+
+    
+    
+
     public static function getList(array $nomColonnes = null, array $conditions = null, string $orderBy = null, string $limit = null, bool $api = false, bool $debug = false)
     {
         $nomColonnes = ($nomColonnes == null) ? Work_technologies::getAttributes() : $nomColonnes;
