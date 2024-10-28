cde79154c55d702e66a25851f1c24823aa4d0714
Val-Del <84442853+Val-Del@users.noreply.github.com>
2024-10-28 13:56:30 -0600

    add technlogies and social

--- PHP/VIEW/GENERAL/Portfolio.php
+++ PHP/VIEW/GENERAL/Portfolio.php
@@ -8,6 +8,13 @@
             <p class="description">
                 Hello! I'm a Full Stack Developer with a strong foundation in application development and web design.
             </p>
+            <a class=os target="_blank" href="?page=Desktop">See my work through a different perspective.</a>
+            <nav class="social">
+                <ul class="flex">
+                    <li>LinkedIn</li>
+                    <li>Github</li>
+                </ul>
+            </nav>
         </section>
         <section class="work flex">
             <?php
@@ -15,12 +22,25 @@
             if ($works) {
                 foreach ($works as $work) {
                     if ($work->getVisible()) {
-                        echo '<div class=card>';
+                        echo '<a href=?page=WorkDetails&id='.$work->getId().' class=card>';
                                 echo '<img src=IMG/'.$work->getThumbnail().'></img>';
                                 echo '<div class=overlay>';
                                     echo '<h4>'.$work->getName().'</h4>';
+                                    $techs = Work_technologiesManager::findTechnologiesByWorkId($work->getId());
+                                    if ($techs) {
+                                        echo '<p class="technologies">';
+                                        $tech = '';
+                                        foreach ($techs as $techItem) {
+                                            $tech .= $techItem['name'] . ', ';
+                                        }
+                                        $tech = rtrim($tech, ', ');
+                                        echo $tech;
+                                        echo '</p>';
+                                    }
+                                    
                                 echo '</div>';
-                        echo '</div>';
+                        echo '</a>';
+                       
                     }
                 }
             }
